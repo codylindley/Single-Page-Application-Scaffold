7ababa8e7950a9fbd334b3cdf99b4c42ead2e288
Mundi Morgado <mundi@tandemseven.com>
2013-11-14 09:46:58 -0800

    added requirejs-handlebars

--- public/require.config.js
+++ public/require.config.js
@@ -5,7 +5,7 @@ requirejs.config({
 		backbone: 'bower_components/backbone/index',
 		text: 'bower_components/requirejs-text/text',
 		handlebars: 'bower_components/handlebars/handlebars.amd',
-		html: 'bower_components/requirejs-handlebars/hbars',
+		html: 'bower_components/requirejs-handlebars/requirejs-handlebars',
 		almond: 'bower_components/almond/almond'
 	}
 });
